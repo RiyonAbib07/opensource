e66cc12c4c048933708d7cb0bf5026c82d880aa0
RiyonAbib07 <riyonabiz7@gmail.com>
2024-11-12 10:26:40 +0530

    Create main.java

---
+++ main.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+
+class Main
+  {
+    public static void main(string[] args)
+    Scanner sc = new Scanner(System.in);
+
+    int n = sc.nextInt();
+
+    if(n==0)
+    {
+      System.out.println("Neither Even nor Odd");
+    }
+    else if(n%2==0)
+    {
+      System.out.println("Even");
+    }
+    else
+    {
+      System.out.println("Odd");
+    }
+  }
